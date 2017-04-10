a5ca305691417684ddb5adead2f7fbef2233a041
zaneta989 <zaneta989@gmail.com>
2017-04-10 16:35:30 +0200

    utworzenie potzebnych aktywnosci i obslga okienka glownego

--- app/src/main/java/com/example/zaneta/zad3_2/MainActivity.java
+++ app/src/main/java/com/example/zaneta/zad3_2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zaneta.zad3_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void wlaczUstawienia(View view) {
+        final Intent intencja2 = new Intent(this,Ustawienia.class);
+        startActivity(intencja2);
+    }
+    public void wlaczWprowadzanie(View view) {
+        final Intent intencja2 = new Intent(this,Wprowadzanie.class);
+        startActivity(intencja2);
+    }
+    public void wlaczListe(View view) {
+        final Intent intencja2 = new Intent(this,Lista.class);
+        startActivity(intencja2);
     }
 }
